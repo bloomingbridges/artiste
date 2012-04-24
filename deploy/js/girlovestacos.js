ef8435eb457a46edfd1063c09f5b1f95e576b998
bloomingbridges <brueckner.florian@googlemail.com>
2012-04-24 18:02:08 +0200

    Souvenirs Asset Library added, clean ups here and there

--- deploy/js/girlovestacos.js
+++ deploy/js/girlovestacos.js
@@ -1,29 +1,36 @@
 
 // Globals
 
-var canvas, ctx, bounds, stage;
-var Assets = {};
-var gir, taco, credits;
+var canvas, ctx, bounds, stage, mouseInside;
 var loader = new PxLoader(); 
 
 
-// States for the Atelier.js StateMachine
+// States for the AtelierJS StateMachine
 
 var IntroState = State.extend({
 
 	init: function(){
 		this._super( false );
 
-		gir.x = bounds.width + 90;
-		gir.y = bounds.height / 2;
-		subStage.addChild(gir);
+		this.gir = Souvenirs.clone('gir');
+		this.subStage.addChild(this.gir);
+		this.gir.x = bounds.width + 90;
+		this.gir.y = bounds.height / 2;
+		this.gir.gotoAndPlay('walk');
 
-		gir.gotoAndPlay('walk');
+		$('#stage').mouseover(function() {
+			mouseInside = true;
+		});
+
+		$('#stage').mouseleave(function() {
+			mouseInside = false;
+		});
+
 	},
 	
 	update: function(){
-		if(gir.x > bounds.width / 2){
-			gir.x -= 2;
+		if(this.gir.x > bounds.width / 2){
+			this.gir.x -= 2;
 		}
 		else {
 			StateMachine.switchTo('PLAY');
@@ -41,30 +48,37 @@ var PlayState = State.extend({
 	init: function() {
 		this._super(false);
 
-		subStage.addChild(gir);
-		gir.gotoAndPlay('idle');
+		this.gir = Souvenirs.clone('gir');
+		this.gir.x = bounds.width / 2;
+		this.gir.y = bounds.height / 2;
+		this.subStage.addChild(this.gir);
+		this.gir.gotoAndPlay('idle');
 		
 		// TODO Use Easel Mouse API instead?
 
-		$('#stage').mouseover(function() {
-			gir.gotoAndPlay('walk');
-		});
-
-		$('#stage').mouseleave(function() {
-			gir.gotoAndStop(0);	
-		});
 		$('#stage').click(function() {
 			$('#stage').removeClass('taco');
+			$('#stage').unbind('click');
 			StateMachine.transitionTo('CREDITS');
 		});
 
 		$('#stage').addClass('taco');
+	},
 
+	update: function() {
+		if(mouseInside){
+			if(this.gir.currentAnimation === 'idle'){
+				this.gir.gotoAndPlay('walk');
+			}
+		}
+		else {
+			this.gir.gotoAndPlay('idle');
+		}
 	},
 
 	disappear: function() {
-		if(gir.x > -180){
-			gir.x -= 2;
+		if(this.gir.x > -180){
+			this.gir.x -= 2;
 		}
 	}
 
@@ -74,57 +88,23 @@ var CreditsState = State.extend({
 
 	init: function(){
 		this._super( false );
-		$('#stage').unbind('mouseover').unbind('mouseleave').unbind('click');
 
-		credits = new Container();
-
-		var shape = new Shape();
-
-		var g = new Graphics();
-		g.beginFill(Graphics.getRGB(176,105,199));
-		g.rect(0,0,bounds.width,bounds.height);
-		shape.graphics = g;
-
-		credits.addChild(shape);
-
-		var text = new Text('Mmmmmm tacos!', 'normal 32px Helvetica', 'white');
-		text.width = bounds.width;
-		text.textAlign = "center";
-		text.x = bounds.width / 2;
-		text.y = bounds.height / 2 - 16;
-
-		var textShadow = new Shadow(Graphics.getRGB(150,20,199), 0, 2, 1);
-		text.shadow = textShadow;
-
-		credits.addChild(text);
-
-		var text2 = new Text('Click to restart', 'normal 16px Helvetica', 'white');
-		text2.width = bounds.width;
-		text2.textAlign = "center";
-		text2.x = bounds.width / 2;
-		text2.y = text.y + 36;
-		text2.alpha = 0.9;
-		credits.addChild(text2);
-
-		credits.x = bounds.width;
-		subStage.addChild(credits);
-
-		$('#stage').click(function() {
-			if(credits.x == 0){
-				$('#stage').unbind('click');
-				Tween.get(credits).to({ y: -bounds.height, alpha: 0.0 }, 1000, Ease.cubicOut).call(function() {
-					gir.x = bounds.width + 180;
-					StateMachine.switchTo('INTRO');
-				});
-			}
-		});
+		this.credits = Souvenirs.clone('credits');
+		this.subStage.x = bounds.width;
+		this.subStage.addChild(this.credits);
 	},
 	
 	appear: function(){
-		if(credits.getStage() != null && credits.x > 0){
-			credits.x -= 2;
+		if(this.subStage.x > 0){
+			this.subStage.x -= 2;
 		}
 		else{
+			$('#stage').click(function() {
+				Tween.get(StateMachine.currentState.credits).to({ y: -bounds.height, alpha: 0.0 }, 1000, Ease.cubicOut).call(function() {
+					$('#stage').unbind('click');
+					StateMachine.switchTo('INTRO');
+				})
+			});
 			StateMachine.endTransition();
 		}
 	},
@@ -145,16 +125,52 @@ var init = function() {
 
 	stage = new Stage(canvas);
 
-	gir = new BitmapAnimation(
+	// Creation of Sprites
+
+	var gir = new BitmapAnimation(
 		new SpriteSheet(
 			{
-				    images: [Assets.girSheetImg],
+				    images: [ Souvenirs.assets.girSheetImg ],
 				    frames: { width: 180, height: 244, count: 9, regX: 90, regY: 122 },
 				animations: { idle: [0], walk: [0,8,true,2] }
 			}
 		)
 	);
 	gir.snapToPixel = true;
+
+	Souvenirs.register('gir', gir);
+
+	var credits = new Container();
+
+	var shape = new Shape();
+
+	var g = new Graphics();
+	g.beginFill(Graphics.getRGB(176,105,199));
+	g.rect(0,0,bounds.width,bounds.height);
+	shape.graphics = g;
+
+	credits.addChild(shape);
+
+	var text = new Text('Mmmmmm tacos!', 'normal 32px Helvetica', 'white');
+	text.width = bounds.width;
+	text.textAlign = "center";
+	text.x = bounds.width / 2;
+	text.y = bounds.height / 2 - 16;
+
+	var textShadow = new Shadow(Graphics.getRGB(150,20,199), 0, 2, 1);
+	text.shadow = textShadow;
+
+	credits.addChild(text);
+
+	var text2 = new Text('Click to restart', 'normal 16px Helvetica', 'white');
+	text2.width = bounds.width;
+	text2.textAlign = "center";
+	text2.x = bounds.width / 2;
+	text2.y = text.y + 36;
+	text2.alpha = 0.9;
+	credits.addChild(text2);
+
+	Souvenirs.register('credits', credits);
 
 	/*  StateMachine.registerStates(stateArray, autoPlay);
 	 *  autoPlay can either be true, false or key in stateArray e.g. 'INTRO'
@@ -182,6 +198,5 @@ function preloadAssets(onComplete) {
 	loader.start(); 
 }
 
-Assets.girSheetImg = loader.addImage('./assets/girsheet.png');
-Assets.tacoImg = loader.addImage('./assets/taco.png');
+Souvenirs.enqueue('girSheetImg', './assets/girsheet.png');
 preloadAssets(false);
--- deploy/js/lib/Atelier.js
+++ deploy/js/lib/Atelier.js
@@ -65,61 +65,93 @@
 
 /* ============================================================================================== */
 
+/* AtelierJS - State Manager (StateMachine) and Asset Library (Souvenirs) classes for CreateJS 
+ * By Florian Brueckner http://bloomingbridges.co.uk/
+ * MIT Licensed.
+ */
+
+var Souvenirs = {
+	 assets: {},
+	sprites: {},
+	enqueue: function(id, path) {	
+		this.assets[id] = loader.addImage(path);
+		console.log(id + ' has been enqueued for preloading.');
+	},
+	retrieve: function(id) {
+		if(id in this.assets){
+			return this.assets[id];
+		}
+		else {
+			throw "Asset doesn't exist :<";
+		}
+	},
+	register: function(id, sprite) {
+		this.sprites[id] = sprite;
+		console.log('Sprite has been registered.');
+	},
+	clone: function(id) {
+		if(id in this.sprites){
+			return this.sprites[id].clone(true);
+		}
+	}
+};
+
 var StateMachine = {
 	states: {},
 	currentState: {},
 	nextState: {},
 	transitioning: false,
 	registerStates: function(stateArray, autoStart) {
-		StateMachine.states = stateArray;
+		this.states = stateArray;
 		if(autoStart){
-			var firstState = (autoStart === true) ? Object.keys(StateMachine.states).shift() : autoStart;
-			StateMachine.switchTo(firstState);
+			var firstState = (autoStart === true) ? Object.keys(this.states).shift() : autoStart;
+			this.switchTo(firstState);
 		}
 	},
 	update: function() {
-		if(StateMachine.currentState instanceof State){
-			if(StateMachine.transitioning === true){
-				StateMachine.currentState.disappear();
-				StateMachine.nextState.appear();
+		if(this.currentState instanceof State){
+			if(this.transitioning === true){
+				this.currentState.disappear();
+				this.nextState.appear();
 			}
-			else if(StateMachine.transitioning === 'ready'){
-				StateMachine.transitioning = false;
+			else if(this.transitioning === 'ready'){
+				this.transitioning = false;
 				console.log('Transition finished!');
 				stage.removeChild(StateMachine.currentState.subStage);
-				StateMachine.currentState = StateMachine.nextState;
+				this.currentState = this.nextState;
 			}
-			StateMachine.currentState.update();
+			else {
+				this.currentState.update();
+			}
 		}
 	},
 	switchTo: function(newState) {
 		console.log('Switching to: ' + newState);
-		if(StateMachine.currentState instanceof State){
-			StateMachine.currentState.destroy();
+		if(this.currentState instanceof State){
+			this.currentState.destroy();
 		}
-		StateMachine.currentState = new StateMachine.states[newState](false);
+		this.currentState = new this.states[newState]();
 	},
 	transitionTo: function(nextState) {
 		console.log('Transitioning to: ' + nextState);
-		StateMachine.nextState = new StateMachine.states[nextState](false);
-		StateMachine.transitioning = true;
+		this.nextState = new this.states[nextState]();
+		this.transitioning = true;
 	},
 	endTransition: function() {
-		StateMachine.transitioning = 'ready';
+		this.transitioning = 'ready';
 	}
 }
 
 var State = Class.extend({
 
-	subStage: {},
-
-	init: function(data) {
+	init: function(blueprint) {
 		
-		subStage = new Container();
-		stage.addChild(subStage);
+		this.subStage = new Container();
+		this.layout = blueprint;
+		stage.addChild(this.subStage);
 
-		if(data && typeof data === 'object'){
-			console.log('constructing state..');		
+		if(blueprint && typeof blueprint === 'object'){
+			// layout assets according to blueprint
 		}
 
 	},
@@ -138,7 +170,7 @@ var State = Class.extend({
 
 	destroy: function() {
 		//subStage.removeAllChildren();
-		stage.removeChild(subStage);
+		stage.removeChild(this.subStage);
 	}
 
 });
